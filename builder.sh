6b798c512b4895b2dcbb4a7341f3669f41bc00dc
thelamer <ryankuba@gmail.com>
2019-02-21 13:18:32 -0800

    fixing endpoint for aarch64 manifests

--- builder.sh
+++ builder.sh
@@ -26,10 +26,10 @@ docker push ${DOCKERHUB_LIVE}:arm64v8-${DOCKER_TAG}
 # Generate local manifests for tag and at commit
 docker manifest create ${DOCKERHUB_LIVE}:${DOCKER_TAG} ${DOCKERHUB_LIVE}:amd64-${DOCKER_TAG} ${DOCKERHUB_LIVE}:arm32v6-${DOCKER_TAG} ${DOCKERHUB_LIVE}:arm64v8-${DOCKER_TAG}
 docker manifest annotate ${DOCKERHUB_LIVE}:${DOCKER_TAG} ${DOCKERHUB_LIVE}:arm32v6-${DOCKER_TAG} --os linux --arch arm
-docker manifest annotate ${DOCKERHUB_LIVE}:${DOCKER_TAG} ${DOCKERHUB_LIVE}:arm64v8-${DOCKER_TAG} --os linux --arch arm64 --variant armv8
+docker manifest annotate ${DOCKERHUB_LIVE}:${DOCKER_TAG} ${DOCKERHUB_LIVE}:arm64v8-${DOCKER_TAG} --os linux --arch arm64 --variant v8
 docker manifest create ${DOCKERHUB_LIVE}:${TRAVIS_COMMIT}-${DOCKER_TAG} ${DOCKERHUB_LIVE}:amd64-${TRAVIS_COMMIT}-${DOCKER_TAG} ${DOCKERHUB_LIVE}:arm32v6-${TRAVIS_COMMIT}-${DOCKER_TAG} ${DOCKERHUB_LIVE}:arm64v8-${TRAVIS_COMMIT}-${DOCKER_TAG}
 docker manifest annotate ${DOCKERHUB_LIVE}:${TRAVIS_COMMIT}-${DOCKER_TAG} ${DOCKERHUB_LIVE}:arm32v6-${TRAVIS_COMMIT}-${DOCKER_TAG} --os linux --arch arm
-docker manifest annotate ${DOCKERHUB_LIVE}:${TRAVIS_COMMIT}-${DOCKER_TAG} ${DOCKERHUB_LIVE}:arm64v8-${TRAVIS_COMMIT}-${DOCKER_TAG} --os linux --arch arm64 --variant armv8
+docker manifest annotate ${DOCKERHUB_LIVE}:${TRAVIS_COMMIT}-${DOCKER_TAG} ${DOCKERHUB_LIVE}:arm64v8-${TRAVIS_COMMIT}-${DOCKER_TAG} --os linux --arch arm64 --variant v8
 # Push the manifests to these meta tags
 docker manifest push ${DOCKERHUB_LIVE}:${DOCKER_TAG}
 docker manifest push ${DOCKERHUB_LIVE}:${TRAVIS_COMMIT}-${DOCKER_TAG}
